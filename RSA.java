053ab308e7eb1bf6f9c68558e0803da8b1dd4fdb
violettelai <xviolette02@gmail.com>
2024-01-03 15:41:52 +0800

    Initial commit

---
+++ RSA.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class RSA{
+    //keygen() = pk,sk
+    //createMsg() = m
+    //enc(m) = c
+    //sendMsg(c) = 1/0
+    //dec(c) = m
+    //viewMsg(m)
+
+    public static void main (String args[]){
+        //welcome page
+        //if(choice = login) login()
+        //else signin()
+    }
+
+    static void login(){
+        //username
+        //pwd
+        //if(match)
+        //User user = new User(username)
+        //user.menu()
+    }
+
+    static void signin(){
+        //username
+        //pwd
+        //User user = new User(username)
+        //user.keygen()
+    }
+}
---
+++ User.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class User{
+    private String username;
+    //pk
+    //sk
+
+    void keygen(){
+        //pk,sk
+    }
+
+    // String encrypt(String m){
+    //     //return c;
+    // }
+
+    // String decrypt(String c){
+    //     //return m;
+    // }
+
+    void menu(){
+        //action
+        //1. send message
+        //2. view message
+        //3. logout
+        //choice
+
+        //if(choice == 1)
+        //createMsg()
+        //else if(choice == 2)
+        //viewMsg()
+        //else
+        //logout()
+    }
+
+    void createMsg(){
+        //send to: receiver
+        //enter your message: xxxxxx... = m
+        //c = encrypt(m)
+        //successfully sent
+        //menu()
+    }
+
+    void viewMsg(){
+        //getmsglist
+        //no  from   date
+        //1.  alice  20240101 23:11
+        //2.  bob    20240102 11:12
+        //c = choice
+        //display(c)
+    }
+
+    void display(String c){
+        //m = decrypt(c)
+        //print(m)
+        //viewMsg()
+    }
+}
